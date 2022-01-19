596dc4cf71790da32816504dbb21ff3d5badb733
Alexandre Brandão Lustosa <alexandre.brandao.lustosa@gmail.com>
2022-01-19 12:02:58 -0300

    Mensagem atualizada

--- appTestQ/Startup.cs
+++ appTestQ/Startup.cs
@@ -32,7 +32,7 @@ namespace appTestQ
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("App Test Q");
                 });
             });
         }
